d95949129a15da52604bdc875a319809a1cb6aeb
Alexey <Leshabiceps34@gmail.com>
2020-10-23 15:57:04 +0600

    Статический метод расширение для счёта возраста.

---
+++ API/Extensions/DateTimeExtansions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Extensions
+{
+    public static class DateTimeExtansions
+    {
+        public static int CalculateAge(this DateTime dateOfBirthday)
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - dateOfBirthday.Year;
+
+            if(dateOfBirthday.Date > today.AddYears(-age)) age--;
+            return age;
+        }
+    }
+}
