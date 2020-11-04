4d11bbacbb49a51a28e84f693a66ea210e741ca5
Alexey <Leshabiceps34@gmail.com>
2020-11-04 14:47:21 +0600

    Возвращает правильный StatusCode при добавление картинки 201.

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -39,7 +39,7 @@ namespace API.Controllers
             return Ok(usersToReturn);
         }
 
-        [HttpGet("{username}")]
+        [HttpGet("{username}", Name = "GetUser")]
         public async Task<ActionResult<MemberDTO>> GetUser(string username)
         {
             return await _userRepository.GetMemberAsync(username);
@@ -82,7 +82,7 @@ namespace API.Controllers
 
             if(await _userRepository.SaveAllAsync())
             {
-                return _mapper.Map<PhotoDto>(photo);
+                return CreatedAtRoute("GetUser", new { username = user.UserName} ,_mapper.Map<PhotoDto>(photo));
             }
 
             return BadRequest("Problem adding photo");
